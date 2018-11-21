5c88143264058ccb28cbb18a27e9d798018ab2f7
mknaya <milan@ingrainhub.com>
2018-11-21 18:20:15 +0530

    new branch

---
+++ src/test/java/TestDemo2.java
@@ -0,0 +1,8 @@
+public class TestDemo2 {
+
+    public static void main(String args[]) {
+        System.out.println("tset1");
+        System.out.println("test2");
+        System.out.println("test3");
+    }
+}
